99e8ddc238e965157bd0fdc88d800e1d12a945f8
Nikita Chuzhakin <mango.syrup@yandex.ru>
2024-10-02 21:32:13 +0300

    rename test_files directorty to fixtures

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,7 +2,7 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    file_path1 = 'tests/test_files/file1.json'
-    file_path2 = 'tests/test_files/file2.json'
-    expected = open('tests/test_files/json_diff_expected.txt').read()
+    file_path1 = 'tests/fixtures/file1.json'
+    file_path2 = 'tests/fixtures/file2.json'
+    expected = open('tests/fixtures/json_diff_expected.txt').read()
     assert generate_diff(file_path1, file_path2) == expected
